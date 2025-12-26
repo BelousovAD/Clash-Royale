96e81002feb22d3e74acae1a0bb714fc1b0995dd
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-12-26 22:05:42 +0800

    Внесены исправления CodeStyle

--- Assets/Scripts/Mediation/AdvertisementButton.cs
+++ Assets/Scripts/Mediation/AdvertisementButton.cs
@@ -1,27 +1,24 @@
+using Bootstrap;
+using Common;
+using Reflex.Attributes;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace Mediation
 {
-    using Bootstrap;
-    using Common;
-    using Reflex.Attributes;
-    using UnityEngine;
-    using UnityEngine.UI;
-
     internal class AdvertisementButton : AbstractButton
     {
         [SerializeField] private Button _buttonToComplete;
         [SerializeField] private bool _isRewarded;
-        
+
         private SavvyServicesProvider _services;
 
         [Inject]
         private void Initialize(SavvyServicesProvider servicesProvider) =>
             _services = servicesProvider;
-        
+
         protected override void HandleClick()
         {
-            HandleComplete();
-            return;
-            
             if (_isRewarded)
             {
                 _services.Mediation.ShowRewardedAd(HandleComplete);
