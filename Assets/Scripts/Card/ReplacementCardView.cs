6fae5b976d557d035555f9a5a1f89bc90062cd58
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2026-01-10 08:10:23 +0800

    Реализована замена карт в контейнерах

---
+++ Assets/Scripts/Card/ReplacementCardView.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Item;
+using Reflex.Attributes;
+using UnityEngine;
+
+namespace Card
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class ReplacementCardView : MonoBehaviour
+    {
+        [SerializeField] private ContainerType _containerTypeToObserve;
+        
+        private CanvasGroup _canvasGroup;
+        private Container _container;
+
+        [Inject]
+        private void Initialize(IEnumerable<Container> containers)
+        {
+            foreach (Container container in containers)
+            {
+                if (container.Type == _containerTypeToObserve)
+                {
+                    _container = container;
+                    break;
+                }
+            }
+        }
+
+        private void Awake() =>
+            _canvasGroup = GetComponent<CanvasGroup>();
+
+        private void OnEnable()
+        {
+            _container.SelectChanged += UpdateView;
+            UpdateView();
+        }
+
+        private void OnDisable() =>
+            _container.SelectChanged -= UpdateView;
+
+        private void UpdateView() =>
+            _canvasGroup.interactable = _container.Selected is not null;
+    }
+}
