3d431ac59cc1a441de13cec8c6666c6503189966
LiveNLy <newrivengod@gmail.com>
2026-01-12 18:40:40 +1000

    Исправлен недочёт в скрипте

--- Assets/Scripts/ChestAnimation/ChestAnimation.cs
+++ Assets/Scripts/ChestAnimation/ChestAnimation.cs
@@ -8,7 +8,7 @@ namespace ChestAnimation
     {
         private const Ease EaseType = Ease.OutBounce;
 
-        [SerializeField][Range(0,180)] private float _openingAngle = 120f;
+        [SerializeField][Range(0, 180)] private float _openingAngle = 120f;
         [SerializeField][Min(0)] private float _animationDuration;
 
         private Transform _lidTransform;
@@ -20,9 +20,7 @@ namespace ChestAnimation
         private void OnDisable() =>
             _tweener?.Kill();
 
-        public void OpenChest()
-        {
+        public void OpenChest() =>
             _tweener = _lidTransform.DOLocalRotate(new Vector3(_openingAngle, 0, 0), _animationDuration).SetEase(EaseType);
-        }
     }
 }
