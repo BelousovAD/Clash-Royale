c73362852700ceb8de8c7423d22abe5e70cad614
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2026-01-16 17:49:35 +0800

    Добавлено отображение статуса победителя

---
+++ Assets/Scripts/Gameplay/VictoryStatusView.cs
@@ -0,0 +1,28 @@
+using Reflex.Attributes;
+using UnityEngine;
+
+namespace Gameplay
+{
+    internal class VictoryStatusView : MonoBehaviour
+    {
+        [SerializeField] private bool _isPlayer;
+        
+        private Judge _judge;
+
+        [Inject]
+        private void Initialize(Judge judge)
+        {
+            _judge = judge;
+            _judge.VictoryStatusChanged += UpdateView;
+        }
+
+        private void OnDestroy() =>
+            _judge.VictoryStatusChanged -= UpdateView;
+
+        private void OnEnable() =>
+            UpdateView();
+
+        private void UpdateView() =>
+            gameObject.SetActive((_judge.IsVictory ^ _isPlayer) == false);
+    }
+}
