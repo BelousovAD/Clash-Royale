cb36ac388d32d841374a337d5c66f7271c0dba0b
LiveNLy <newrivengod@gmail.com>
2026-01-29 16:56:02 +1000

    Дополнена анимации сломаной башни

--- Assets/Scripts/Tower/TopCastleMove.cs
+++ Assets/Scripts/Tower/TopCastleMove.cs
@@ -5,6 +5,9 @@ namespace Tower
 {
     public class TopCastleMove : MonoBehaviour
     {
+        private const float Duration = 1;
+        private const float Delay = 1.88f;
+        
         [SerializeField] private Transform _endPoint;
         
         private Sequence _sequence;
@@ -22,8 +25,8 @@ namespace Tower
         private void Move()
         {
             _sequence = DOTween.Sequence();
-            _sequence.Append(transform.DOMove(_endPoint.position, 2));
-            _sequence.Append(transform.DORotate(_endPoint.rotation.eulerAngles, 2));
+            _sequence.Append(transform.DOMove(_endPoint.position, Duration)).SetDelay(Delay);
+            _sequence.Join(transform.DORotate(_endPoint.rotation.eulerAngles, Duration).SetDelay(Delay));
         }
     }
 }
