5d923f74c348ce2947ab82fefb9a92cf0ad78ac9
LiveNLy <newrivengod@gmail.com>
2026-02-02 20:37:48 +1000

    Добавлены проджектайла моделям

--- Assets/Scripts/Animator/ProjectileAnimation.cs
+++ Assets/Scripts/Animator/ProjectileAnimation.cs
@@ -4,15 +4,19 @@ namespace Animator
 {
     internal class ProjectileAnimation : MonoBehaviour
     {
-        private const float Speed = 500;
-        
+        private const float Speed = 1500;
+
         [SerializeField] private GameObject _projectile;
         [SerializeField] private Transform _spawnPosition;
 
         public void SendProjectile()
         {
             GameObject projectile = Instantiate(_projectile, _spawnPosition.position, Quaternion.identity) as GameObject;
-            projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * Speed);
+            
+            if (projectile.TryGetComponent<Rigidbody>(out Rigidbody rigidbody))
+            {
+                rigidbody.AddForce(projectile.transform.forward * Speed);
+            }
         }
     }
 }
