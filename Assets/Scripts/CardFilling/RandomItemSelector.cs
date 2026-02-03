b244c8141f6c67af3bcf1931ea4c7ac36786d90e
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2026-02-03 17:07:13 +0800

    Переименован модуль CardBattle в CardFilling и реализован функционал для заполнения контейнера карт руки оппонента

---
+++ Assets/Scripts/CardFilling/RandomItemSelector.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Item;
+using Random = UnityEngine.Random;
+
+namespace CardFilling
+{
+    internal static class RandomItemSelector
+    {
+        public static void Select(Container container, IEnumerable<string> excludeSubtypes = null)
+        {
+            List<Item.Item> items = new (container.Items);
+            List<string> excludes = excludeSubtypes != null ? new List<string>(excludeSubtypes) : new List<string>();
+
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                if (excludes.Contains(items[i].Subtype))
+                {
+                    items.RemoveAt(i);
+                }
+            }
+            
+            items[Random.Range(0, items.Count)].Select();
+        }
+    }
+}
--- Assets/Scripts/Item/ContainerType.cs
+++ Assets/Scripts/Item/ContainerType.cs
@@ -7,5 +7,7 @@ namespace Item
         Chest = 2,
         Character = 3,
         HandCard = 4,
+        EnemyEquippedCard = 5,
+        EnemyHandCard = 6,
     }
 }
