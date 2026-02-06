cddf8947fd3127609ce8b1ca6bbf349400777433
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2026-02-07 02:15:35 +0800

    Исправлено обновление Healthbar

--- Assets/Scripts/UnitHealth/HealthSmoothSliderView.cs
+++ Assets/Scripts/UnitHealth/HealthSmoothSliderView.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,12 +9,19 @@ namespace UnitHealth
         [SerializeField] private float _changingTime = 1f;
 
         private Slider _slider;
-        private float _targetValue;
+        private float _targetValue = 1f;
         private float _changingSpeed;
-        private Coroutine _changingValueSmoothly;
 
         private void Awake() =>
             _slider = GetComponent<Slider>();
+
+        private void Update()
+        {
+            if (Mathf.Approximately(_targetValue, _slider.value) == false)
+            {
+                _slider.value = Mathf.MoveTowards(_slider.value, _targetValue, Time.deltaTime * _changingSpeed);
+            }
+        }
 
         protected override void UpdateView()
         {
@@ -23,26 +29,7 @@ namespace UnitHealth
             {
                 _targetValue = Health.Value / Health.MaxValue;
                 _changingSpeed = Mathf.Abs(_targetValue - _slider.value) / _changingTime;
-                _changingValueSmoothly ??= StartCoroutine(ChangeValueSmoothly());
             }
-            else if (_changingValueSmoothly is not null)
-            {
-                StopCoroutine(_changingValueSmoothly);
-                _changingValueSmoothly = null;
-            }
-        }
-        
-        private IEnumerator ChangeValueSmoothly()
-        {
-            while (isActiveAndEnabled && Mathf.Approximately(_targetValue, _slider.value) == false)
-            {
-                _slider.value = Mathf.MoveTowards(_slider.value, _targetValue, Time.deltaTime * _changingSpeed);
-
-                yield return null;
-            }
-
-            _slider.value = _targetValue;
-            _changingValueSmoothly = null;
         }
     }
 }
